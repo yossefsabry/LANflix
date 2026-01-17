c16b73dad262350fe428dec5c30f43d9dc2d989b
yossefsabry <yossefsabry66@gmail.com>
2026-01-18 01:53:18 +0200

    adding new version updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.thiyagu.media_server"
         minSdk = 21
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "1.0.3"
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
