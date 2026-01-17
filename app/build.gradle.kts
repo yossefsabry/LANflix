59d2a9ca141130802f4c4a79d07041f2c9e2faf2
yossefsabry <yossefsabry66@gmail.com>
2026-01-18 00:48:42 +0200

    Bump versionCode to 3 for F-Droid

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.thiyagu.media_server"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
